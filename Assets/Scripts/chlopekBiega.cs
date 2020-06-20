34e530108b511a64c5dc8dba779805bfb4536f2c
alanjaskot <alanjaskot@gmail.com>
2020-06-20 10:13:37 +0200

    Initial commit

---
+++ Assets/Scripts/chlopekBiega.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class chlopekBiega : MonoBehaviour
+{
+    private float minX = -2.8f;
+    private float maxX = 2.8f;
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    float speed = 3f;
+
+    public Text timer_Text;
+    private int timer;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        StartCoroutine(CountTime());
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        PlayerBounce();
+    }
+
+    void PlayerBounce()
+    {
+        Vector3 temp = transform.position;
+
+        if( temp.x > maxX)
+        {
+            temp.x = maxX;
+        }
+        else if (temp.x < minX)
+        {
+            temp.x = minX;
+        }
+
+        transform.position = temp;
+
+    }
+
+    public void move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector3 temp = transform.position;
+
+        if (h > 0)
+        {
+            temp.x += speed * Time.deltaTime;
+            sr.flipX = false;
+            anim.SetBool("chodzi", true);
+        }
+        else if (h < 0)
+        {
+            temp.x -= speed * Time.deltaTime;
+            sr.flipX = true;
+            anim.SetBool("chodzi", true);
+        }
+        else if (h == 0)
+        {
+            anim.SetBool("chodzi", false);
+        }
+
+        transform.position = temp;
+    }
+
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator CountTime()
+    {
+        yield return new WaitForSeconds(1f);
+        timer++;
+
+        timer_Text.text = "Czas:  " + timer;
+        StartCoroutine(CountTime());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Knife")
+        {
+            Time.timeScale = 0f;
+            StartCoroutine(RestartGame());
+        }
+    }
+}
---
+++ Assets/Scripts/spawnerNozy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class spawnerNozy : MonoBehaviour
+{ 
+    public GameObject knife;
+    private float minX = -2.8f;
+    private float maxX = 2.8f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(zacznijSpawn());
+    }
+    
+    IEnumerator zacznijSpawn()
+    {
+        yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+
+        GameObject k = Instantiate(knife);
+
+        float x = Random.Range(minX, maxX);
+
+        k.transform.position = new Vector2(x, transform.position.y);
+
+        StartCoroutine(zacznijSpawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
